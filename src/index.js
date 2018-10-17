eefbdd4c37aea5cb425840c358ecc8a28ad81190
Hernán Herreros Niño <hernan.herreros.13@sansano.usm.cl>
2018-10-17 17:04:11 -0300

    Precompile to ES5

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+
 /**
  * React Facebook Pixel Module
  *
@@ -6,14 +14,14 @@
  */
 
 //
-let initialized = false;
-let debug = false;
+var initialized = false;
+var debug = false;
 
 /**
  * Utilities
  */
 
-const verifyInit = () => {
+var verifyInit = function verifyInit() {
   if (!initialized) {
     console.warn('Pixel not initialized before using call ReactPixel.init with required params');
   }
@@ -21,31 +29,38 @@ const verifyInit = () => {
 };
 
 //
-const log = (...args) => {
-  console.info(...['[react-facebook-pixel]'].concat(args));
+var log = function log() {
+  var _console;
+
+  for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
+    args[_key] = arguments[_key];
+  }
+
+  (_console = console).info.apply(_console, _toConsumableArray(['[react-facebook-pixel]'].concat(args)));
 };
 
 //
-const defaultOptions = {
+var defaultOptions = {
   autoConfig: true,
   debug: false
 };
 
 //
-export default {
-  init(pixelId, advancedMatching = {}, options = defaultOptions) {
+exports.default = {
+  init: function init(pixelId) {
+    var advancedMatching = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
+    var options = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : defaultOptions;
+
     /* eslint-disable */
     !function (f, b, e, v, n, t, s) {
-      if (f.fbq) return; n = f.fbq = function () {
-        n.callMethod ?
-          n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+      if (f.fbq) return;n = f.fbq = function () {
+        n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments);
       };
-      if (!f._fbq) f._fbq = n; n.push = n; n.loaded = !0; n.version = '2.0';
-      n.queue = []; t = b.createElement(e); t.async = !0;
-      t.src = v; s = b.getElementsByTagName(e)[0];
-      s.parentNode.insertBefore(t, s)
-    }(window, document, 'script',
-      'https://connect.facebook.net/en_US/fbevents.js');
+      if (!f._fbq) f._fbq = n;n.push = n;n.loaded = !0;n.version = '2.0';
+      n.queue = [];t = b.createElement(e);t.async = !0;
+      t.src = v;s = b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t, s);
+    }(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
     /* eslint-enable */
 
     if (!pixelId) {
@@ -61,8 +76,7 @@ export default {
       debug = options.debug;
     }
   },
-
-  pageView() {
+  pageView: function pageView() {
     if (!verifyInit()) {
       return;
     }
@@ -73,8 +87,7 @@ export default {
       log('called fbq(\'track\', \'PageView\');');
     }
   },
-
-  track(title, data) {
+  track: function track(title, data) {
     if (!verifyInit()) {
       return;
     }
@@ -82,15 +95,14 @@ export default {
     fbq('track', title, data); // eslint-disable-line no-undef
 
     if (debug) {
-      log(`called fbq('track', '${title}');`);
+      log('called fbq(\'track\', \'' + title + '\');');
 
       if (data) {
         log('with data', data);
       }
     }
   },
-
-  trackCustom(event, data) {
+  trackCustom: function trackCustom(event, data) {
     if (!verifyInit()) {
       return;
     }
@@ -98,27 +110,40 @@ export default {
     fbq('trackCustom', event, data); // eslint-disable-line no-undef
 
     if (debug) {
-      log(`called fbq('trackCustom', '${event}');`);
+      log('called fbq(\'trackCustom\', \'' + event + '\');');
 
       if (data) {
         log('with data', data);
       }
     }
   },
+  fbq: function (_fbq) {
+    function fbq() {
+      return _fbq.apply(this, arguments);
+    }
 
-  fbq(...args) {
+    fbq.toString = function () {
+      return _fbq.toString();
+    };
+
+    return fbq;
+  }(function () {
     if (!verifyInit()) {
       return;
     }
 
-    fbq(...args); // eslint-disable-line no-undef
+    for (var _len2 = arguments.length, args = Array(_len2), _key2 = 0; _key2 < _len2; _key2++) {
+      args[_key2] = arguments[_key2];
+    }
+
+    fbq.apply(undefined, args); // eslint-disable-line no-undef
 
     if (debug) {
-      log(`called fbq('${args.slice(0, 2).join('\', \'')}')`);
+      log('called fbq(\'' + args.slice(0, 2).join('\', \'') + '\')');
 
       if (args[2]) {
         log('with data', args[2]);
       }
     }
-  },
+  })
 };
